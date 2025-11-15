c84e1de7d86d21bb0036b50c42a7d9a8191b1035
Joshua Treudler <joshua@treudler.net>
2025-11-15 16:00:06 +0100

    Enhance alert visibility on checkout page: add 'alert-permanent' class to error and success messages

--- public/checkout.php
+++ public/checkout.php
@@ -148,13 +148,13 @@ renderHeader('Checkout', $session);
 <?php else: ?>
 
     <?php if ($error): ?>
-        <div class="alert alert-danger"><?php echo e($error); ?></div>
+        <div class="alert alert-danger alert-permanent"><?php echo e($error); ?></div>
     <?php endif; ?>
 
     <h1 class="mb-4">Checkout</h1>
 
     <!-- Delivery Time Notice -->
-    <div class="alert alert-success mb-4">
+    <div class="alert alert-success mb-4 alert-permanent">
         <h6 class="alert-heading mb-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16" style="vertical-align: text-bottom;">
                 <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -226,7 +226,7 @@ renderHeader('Checkout', $session);
                             </div>
                         </div>
 
-                        <div class="alert alert-info">
+                        <div class="alert alert-info alert-permanent">
                             <p class="mb-1">
                                 After placing your order you can optionally submit your Discord handle or character name so we can deliver even faster.
                             </p>
